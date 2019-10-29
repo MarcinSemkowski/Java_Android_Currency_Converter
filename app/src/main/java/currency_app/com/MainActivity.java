a76de90e0750362aa7ae77617584b3563b824443
Marcin <marcin96semkowski@gmail.com>
2019-10-29 16:41:04 +0100

    Currency #1

--- app/src/main/java/currency_app/com/MainActivity.java
+++ app/src/main/java/currency_app/com/MainActivity.java
@@ -3,8 +3,18 @@ package currency_app.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void convert(View view){
+        EditText euroAmount = findViewById(R.id.AmountEuro);
+        Double euroDouble = Double.parseDouble(euroAmount.getText().toString());
+        Double amountShow = euroDouble *  4.26549;
+        Toast.makeText(MainActivity.this, amountShow + "PLN", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
